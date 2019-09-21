95839523b73d5fe18febe3e9b615b2ee99f7fd01
Jinhui Zhu <jinhuizhu@gmail.com>
2019-09-21 23:59:01 +0800

    add Main class to enable gradle boot
    since gradle build for Spring Boot requires one single main class

---
+++ src/main/java/com/zjh/learning/Main.java
@@ -0,0 +1,19 @@
+package com.zjh.learning;
+/**
+ * Copyright (c) Jinhui Zhu (jinhuizhu@gmail.com)
+ * All rights reserved.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jinhui Zhu (jinhuizhu@gmail.com)
+ * created at:  9/21/2019 11:52 PM
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
